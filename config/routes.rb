4592edc28932f4fa8e4a406ee7772a25d964898a
Nathan Hutton <nathan_hutton@yahoo.com>
2017-06-28 14:34:09 +0000

    Implemented edit and update functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  
   resources :portfolios
-  
   get 'pages/home'
 
   get 'pages/about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,8 +21,8 @@ puts "5 skills created"
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "My great service",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "https://www.google.com/search?q=watermelon+guy&safe=strict&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiPtpi6od7UAhUKhlQKHTN0AAYQ_AUIBigB&biw=1920&bih=974#imgrc=SzWJW76yUIYtWM:",
-    thumb_image: "https://www.google.com/search?q=watermelon+guy&safe=strict&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiPtpi6od7UAhUKhlQKHTN0AAYQ_AUIBigB&biw=1920&bih=974#imgrc=SzWJW76yUIYtWM:"
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
   )
 end
 
