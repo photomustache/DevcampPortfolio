9d7cc2d5babff49f007e01f9efbdfe98db966bd7
Nathan Hutton <nathan_hutton@yahoo.com>
2017-07-12 15:02:27 +0000

    implemented validations for blogs, skills, and portfolio items, and fixed other crap

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -25,7 +25,7 @@ class PortfoliosController < ApplicationController
 
   def update
     @portfolio_item = Portfolio.find(params[:id])
-    
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -34,21 +34,22 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
+
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
-  
+
   def destroy
     # Perform the lookup
     @portfolio_item = Portfolio.find(params[:id])
-    
+
     # Destroy/delete the record
     @portfolio_item.destroy
-    
-    #Redirect
+
+    # Redirect
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Post was removed.' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
+
 end
