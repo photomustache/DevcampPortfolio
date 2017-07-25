6936524b6d91aade7aa2a12c38ed1b1a78d5ff1a
Nathan Hutton <nathan_hutton@yahoo.com>
2017-07-25 15:39:59 +0000

    Completed authentication with virtual attrs for user model

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  
-  get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#about'
-  
+
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :blogs do
     member do
       get :toggle_status
